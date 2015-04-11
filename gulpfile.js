63dd4eecc01fdeef557f6cd66e99172d908ada01
Aditya Pratap Singh <adisinghrajput@gmail.com>
2015-04-11 15:46:40 +0530

    Add new tasks for less, watchers

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,33 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var ext_replace = require('gulp-ext-replace');
 
-gulp.task('default', function () {
+// Style dependencies
+var less = require('gulp-less');
+var prefix = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+
+// Setup tasks for styles
+gulp.task('styles', function() {
+    return gulp.src('app/styles/*.less')
+		      .pipe(less())
+		      .pipe(prefix({cascade: true}))
+		      .pipe(ext_replace(".css"))
+		      .pipe(gulp.dest('app/styles'));
+});
+
+// Setup tasks for converting  jsx to js
+gulp.task('browserify-jsx', function () {
     return gulp.src("app/**/*.jsx")
-                .pipe(babel())
+            	.pipe(babel())
                 .pipe(ext_replace(".js"))
                 .pipe(gulp.dest("app"))
 });
+
+// Watcher task
+gulp.task('watch',function() {
+   gulp.watch('app/**/*.jsx', ['browserify-jsx']);
+   gulp.watch('app/styles/*.less', ['styles']);
+});
+
+// Default task
+gulp.task('default', ['watch']);
