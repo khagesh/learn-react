19ab5bb302a8f34f101fa19225a894f603c51352
Aditya Pratap Singh <adisinghrajput@gmail.com>
2015-04-19 02:24:22 +0530

    Generate templates and theme combination

--- app/Home/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Khagesh.Sharma on 4/11/2015.
- */
-import React from 'react'
-import { Hello } from '../components/Hello'
-
-React.render(<Hello name="All" />, document.querySelector(".react-container"));
--- app/scripts/home/index.jsx
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var React = require('react');
-var PricingTemplate1 = require('./PricingTemplate1');
-var PricingTemplate1Container = require('./PricingTemplate1Container');
-
-
-React.render(<PricingTemplate1Container />, document.getElementById('react-container'));
